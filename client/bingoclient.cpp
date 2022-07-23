48f38fb2db011bd77e25714f703de5cfff11a6a9
abhilashraju <abhilash.kollam@gmail.com>
2022-07-23 18:52:29 +0530

    Add files via upload

---
+++ client/bingoclient.cpp
@@ -0,0 +1,28 @@
+// Client side C/C++ program to demonstrate Socket
+// programming
+#include "bingo.hpp"
+int PORT=8089;
+using namespace bingo;
+int main(int argc, char const* argv[])
+{
+          
+    sock_stream client;
+    auto newstream=connect(client,{"127.0.0.1",PORT});
+    while (true)
+    {
+        std::string str("hello from me");
+        Buffer tosend{str.data(),str.length()};
+        send(client,tosend);
+        std::array<char,1024> arry{0};
+        Buffer buffer{arry.data(),arry.size()};
+        int bytes= read(client,buffer);
+        auto v=buffer.buffer();
+        std::printf("%s",v);
+    }
+    
+   
+
+    
+   
+    return 0;
+}
---
+++ server/bingoserver.cpp
@@ -0,0 +1,35 @@
+// Server side C/C++ program to demonstrate Socket
+// programming
+#include "bingo.hpp"
+#define PORT 8089
+
+using namespace bingo;
+
+
+
+int main(int argc , char const* argv[])
+{
+  (void)argc;(void)argv;
+  unifex::static_thread_pool context;
+  
+      
+      auto server=make_listener("127.0.0.1",PORT)
+                          | process_clients(context,make_request_handler([](Buffer buff){return buff;}));
+      unifex::sync_wait(server);
+      return 0;
+    
+    // sock_stream listener;
+    // listener.bind({"127.0.0.1",PORT});
+    // listen(listener);
+    // while(true){
+    //     sock_address foriengAddr;
+    //     auto incoming = accept(listener);
+    //     std::vector<char> vec;vec.reserve(1024);
+    //     Buffer buffer{vec.data(),vec.capacity()};
+    //     int bytes = read(incoming,buffer);
+    //     auto v=buffer.buffer();
+    //     send(incoming,buffer);
+    // }
+    
+    return 0;
+}

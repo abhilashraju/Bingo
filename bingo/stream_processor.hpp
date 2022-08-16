9813f218bae8bd5858933eee6d27a8992c1ed7aa
abhilashraju <abhilash.r3@hp.com>
2022-08-16 20:45:58 +0530

    adding stream_processor

---
+++ bingo/stream_processor.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "unifex/inplace_stop_token.hpp"
+#include "unifex/repeat_effect_until.hpp"
+#include "unifex/then.hpp"
+#include "unifex/sequence.hpp"
+#include "buffer.hpp"
+#include "bingo.hpp"
+namespace bingo {
+template <typename Stream, typename Handler> struct stream_processor {
+
+  Stream &stream;
+  unifex::inplace_stop_source stop_src;
+  Handler handler;
+  std::vector<char> v;
+  vector_buffer buf{v};
+  stream_processor(Stream &s, Handler h) : stream(s), handler(std::move(h)) {}
+  void request_stop() { stop_src.request_stop(); }
+  auto handle_error(std::exception &e) {
+    buf.consume_all();
+    auto len = strlen(e.what());
+    std::copy_n(e.what(), len, buf.prepare(len));
+    buf.commit(len);
+    stop_src.request_stop();
+    return buf.read_view();
+  }
+  friend auto operator|(auto sender, stream_processor &processor) {
+
+    auto wait_for_remote = [&]() {
+      try {
+        return processor.stream.sync_read(
+            processor.buf, [&]() { return processor.buf.read_view(); });
+      } catch (std::exception &e) {
+        return processor.handle_error(e);
+      }
+    };
+    auto readSender = sender | unifex::then(wait_for_remote);
+    auto processingSender = processor.handler(readSender);
+    return processingSender |
+           unifex::then([&]() { processor.buf.consume_all(); }) |
+           unifex::repeat_effect_until([&]() {
+             return processor.stop_src.get_token().stop_requested();
+           });
+  }
+};
+} // namespace bingo

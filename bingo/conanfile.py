d8f98e25b841164ad5d29c88b3dee6a3936494fb
abhilashraju <abhilash.kollam@gmail.com>
2023-04-20 18:07:17 +0530

    Update conanfile.py

--- bingo/conanfile.py
+++ bingo/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 
 class BongoConan(ConanFile):
-    name = "Bingo"
+    name = "bingo"
     version = "01"
 
     # Optional metadata
